0b87f5249ada3c8c40562ff19ae983c90e851417
beka lekishvili <58632301+Tilted-One@users.noreply.github.com>
2024-09-30 01:04:08 +0400

    fix

--- src/App.js
+++ src/App.js
@@ -112,10 +112,9 @@ export default function App() {
             holdDice={() => holdDice(die.id)}
         />
     ))
-
     return (
         <main>
-            {tenzies && <Confetti />}
+            {tenzies && <Confetti style={{width: '100%', height: '100%'}}/>}
             <h1 className="title">Tenzies</h1>
             <p className="instructions">Roll until all dice are the same.
                 Click each die to freeze it at its current value between rolls.</p>
